c62a01aac21787eb758817dc32322ceefc7d87db
wojtekka <wojtekka@907b033a-e947-4f26-a2d4-362df6eaea90>
2006-04-15 17:58:19 +0000

    Usunięcie Makefile.in.

--- autoclean.sh
+++ autoclean.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-rm -rf config.* autom4te.cache Makefile.in Makefile INSTALL aclocal.m4 depcomp install-sh missing mkinstalldirs stamp* configure confdefs.h src/.deps src/.libs src/*.o src/Makefile.in src/Makefile src/config.h install-sh
+rm -rf config.* autom4te.cache Makefile.in Makefile INSTALL aclocal.m4 depcomp install-sh missing mkinstalldirs stamp* configure confdefs.h src/.deps src/.libs src/*.o src/Makefile.in src/Makefile src/config.h install-sh ltmain.sh compile
+
