1c252fd4cb99de31a952610b0fbb010a71b91789
wojtekka <wojtekka@907b033a-e947-4f26-a2d4-362df6eaea90>
2007-02-26 16:47:36 +0000

    Porządki.

--- autoclean.sh
+++ autoclean.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rm -rf config.* autom4te.cache Makefile.in Makefile INSTALL aclocal.m4 depcomp install-sh missing mkinstalldirs stamp* configure confdefs.h src/.deps src/.libs src/*.o src/Makefile.in src/Makefile src/config.h install-sh ltmain.sh compile libtool *~
+rm -rf config.* autom4te.cache Makefile.in Makefile INSTALL aclocal.m4 depcomp install-sh missing stamp* configure confdefs.h src/.deps src/.libs src/*.o src/Makefile.in src/Makefile src/config.h install-sh ltmain.sh compile libtool *~
